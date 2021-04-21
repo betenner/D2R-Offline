2388d1c372351eca88dd5e66f2dee8542fc8ddda
Łukasz Ciesielski <l.ciesielski0@gmail.com>
2021-04-21 23:08:33 +0200

    Save Patcher Improvements

---
+++ src/Tools/Constants.cs
@@ -0,0 +1,45 @@
+﻿namespace D2ROffline.Tools
+{
+    internal class Constants
+    {
+        public const int GAME_COMPLETED_ON_HELL = 0x0F;
+
+        public const int CHARACTER_PROGRESSION_OFFSET = 0x25;
+        public const int CHECKSUM_OFFSET = 0x0C;
+
+        public const int QUESTS_SECTION_OFFSET = 0x014F;
+
+        public const int WAYPOINTS_SECTION_OFFSET = 0x0279;
+        public const int WAYPOINTS_DATA_OFFSET = 0x08;
+        public const int WAYPOINTS_DIFFICULTY_OFFSET = 0x18;
+
+        public const string DIABLO_SAVE_FILE_EXTENSION = ".d2s";
+        public const string DIABLO_DEFAULT_SAVE_FOLDER = "Diablo II Resurrected Tech Alpha";
+
+        public enum Difficulty
+        {
+            Normal,
+            Nightmare,
+            Hell
+        }
+
+        public enum Act
+        {
+            TheSightlessEye,
+            SecretOfTheVizjerei,
+            TheInfernalGate,
+            TheHarrowing,
+            LordOfDestruction
+        };
+
+        public enum Quest
+        {
+            FirstQuest,
+            SecondQuest,
+            ThirdQuest,
+            FourthQuest,
+            FifthQuest,
+            SixthQuest
+        };
+    }
+}
--- src/Tools/SaveFilePatcher.cs
+++ src/Tools/SaveFilePatcher.cs
@@ -6,13 +6,6 @@ namespace D2ROffline.Tools
 {
     internal class SaveFilePatcher : NativeMethods
     {
-        const int CHARACTER_PROGRESSION_OFFSET = 0x25;
-        const int GAME_FINISHED_ON_HELL = 0x0F;
-        const int CHECKSUM_OFFSET = 0x0C;
-
-        const string DIABLO_SAVE_FILE_EXTENSION = ".d2s";
-        const string DIABLO_DEFAULT_SAVE_FOLDER = "Diablo II Resurrected Tech Alpha";
-
         public static void PatchSaveFiles(string saveFileName)
         {
             Program.ConsolePrint("Patching save files...");
@@ -31,16 +24,16 @@ namespace D2ROffline.Tools
                 return;
             }
 
-            savedGamesPath = Path.Combine(savedGamesPath, DIABLO_DEFAULT_SAVE_FOLDER);
+            savedGamesPath = Path.Combine(savedGamesPath, Constants.DIABLO_DEFAULT_SAVE_FOLDER);
 
             string searchPattern;
             if (string.IsNullOrWhiteSpace(saveFileName) || saveFileName.Equals("*"))
             {
-                searchPattern = "*" + DIABLO_SAVE_FILE_EXTENSION;
+                searchPattern = "*" + Constants.DIABLO_SAVE_FILE_EXTENSION;
             }
             else 
             {
-                searchPattern = saveFileName + DIABLO_SAVE_FILE_EXTENSION;
+                searchPattern = saveFileName + Constants.DIABLO_SAVE_FILE_EXTENSION;
             }
 
             string[] saveFiles = Directory.GetFiles(savedGamesPath, searchPattern);
@@ -60,31 +53,137 @@ namespace D2ROffline.Tools
         private static void ProcessSaveFile(string saveFileAbsolutePath)
         {
             string saveFileName = Path.GetFileName(saveFileAbsolutePath);
-            byte[] saveFile = File.ReadAllBytes(saveFileAbsolutePath);
+            byte[] rawSaveFile = File.ReadAllBytes(saveFileAbsolutePath);
 
-            if (saveFile[CHARACTER_PROGRESSION_OFFSET] == GAME_FINISHED_ON_HELL)
-            {
-                Program.ConsolePrint($"{saveFileName} already patched, skipping save file");
-            }
-            else
-            {
-                File.WriteAllBytes(saveFileAbsolutePath + ".backup", saveFile);
-                Program.ConsolePrint($"Backup for {saveFileName} created");
+            File.WriteAllBytes(saveFileAbsolutePath + ".backup", rawSaveFile);
+            Program.ConsolePrint($"Backup for {saveFileName} created");
 
-                saveFile[CHARACTER_PROGRESSION_OFFSET] = GAME_FINISHED_ON_HELL;
-                UpdateChecksum(saveFile);
-                File.WriteAllBytes(saveFileAbsolutePath, saveFile);
-                Program.ConsolePrint($"{saveFileName} patched successfully");
+            EnableAllWaypoints(rawSaveFile);
+            CompleteGame(rawSaveFile);
+
+            UpdateChecksum(rawSaveFile);
+
+            File.WriteAllBytes(saveFileAbsolutePath, rawSaveFile);
+            Program.ConsolePrint($"{saveFileName} patched successfully");
+        }
+
+        // credits for below methods : ternence-li & VoidSt4r : https://github.com/ternence-li/Diablo2HeroEditor
+        private static void EnableAllWaypoints(byte[] rawSaveFile)
+        {
+            for (int difficulty = 0; difficulty < 3; difficulty++)
+            {
+                int firstWpOffset = Constants.WAYPOINTS_SECTION_OFFSET + 
+                    (Constants.WAYPOINTS_DATA_OFFSET + difficulty * Constants.WAYPOINTS_DIFFICULTY_OFFSET);
+                rawSaveFile[firstWpOffset + 2] = 0xFF;
+                rawSaveFile[firstWpOffset + 3] = 0xFF;
+                rawSaveFile[firstWpOffset + 4] = 0xFF;
+                rawSaveFile[firstWpOffset + 5] = 0xFF;
+                rawSaveFile[firstWpOffset + 6] = 0x7F;
             }
         }
 
-        // credits ternence-li & VoidSt4r : https://github.com/ternence-li/Diablo2HeroEditor/blob/18b7633c437c8da6a1f8b5797b126458489e8bc5/Diablo2FileFormat/Checksum.cs
+        private static void ChangeQuests(Constants.Difficulty difficulty, Constants.Act act, bool complete, byte[] rawSaveFile)
+        {
+            ChangeQuest(difficulty, act, Constants.Quest.FirstQuest, complete, rawSaveFile);
+            ChangeQuest(difficulty, act, Constants.Quest.SecondQuest, complete, rawSaveFile);
+            ChangeQuest(difficulty, act, Constants.Quest.ThirdQuest, complete, rawSaveFile);
+            if (act != Constants.Act.TheHarrowing)
+            {
+                ChangeQuest(difficulty, act, Constants.Quest.FourthQuest, complete, rawSaveFile);
+                ChangeQuest(difficulty, act, Constants.Quest.FifthQuest, complete, rawSaveFile);
+                ChangeQuest(difficulty, act, Constants.Quest.SixthQuest, complete, rawSaveFile);
+            }
+        }
+
+        private static void ChangeQuests(Constants.Difficulty difficulty, bool complete, byte[] rawSaveFile)
+        {
+            ChangeQuests(difficulty, Constants.Act.TheSightlessEye, complete, rawSaveFile);
+            ChangeQuests(difficulty, Constants.Act.SecretOfTheVizjerei, complete, rawSaveFile);
+            ChangeQuests(difficulty, Constants.Act.TheInfernalGate, complete, rawSaveFile);
+            ChangeQuests(difficulty, Constants.Act.TheHarrowing, complete, rawSaveFile);
+            ChangeQuests(difficulty, Constants.Act.LordOfDestruction, complete, rawSaveFile);
+        }
+        private static void ChangeQuests(bool complete, byte[] rawSaveFile)
+        {
+            ChangeQuests(Constants.Difficulty.Normal, complete, rawSaveFile);
+            ChangeQuests(Constants.Difficulty.Nightmare, complete, rawSaveFile);
+            ChangeQuests(Constants.Difficulty.Hell, complete, rawSaveFile);
+        }
+
+        private static void CompleteGame(byte[] rawSaveFile) 
+        {
+            rawSaveFile[Constants.CHARACTER_PROGRESSION_OFFSET] = Constants.GAME_COMPLETED_ON_HELL;
+            ChangeQuests(true, rawSaveFile);
+        }
+
+        private static int GetQuestOffset(Constants.Difficulty difficulty, Constants.Act act, Constants.Quest quest)
+        {
+            int offset = -1;
+
+            if (act != Constants.Act.TheHarrowing || quest < Constants.Quest.FourthQuest)
+            {
+                offset = 12;                    // 10 bytes for the quest header, 2 bytes for the act introduction
+
+                offset += (int)difficulty * 96; // choose to the right difficulty
+                offset += (int)act * 16;        // choose to the right act
+                offset += (int)quest * 2;       // choose the right quest
+
+                if (act == Constants.Act.LordOfDestruction)
+                {
+                    offset += 4;                // there are additional bytes in act 4
+                }
+            }
+
+            return offset;
+        }
+
+        private static void ChangeQuest(Constants.Difficulty difficulty, Constants.Act act, Constants.Quest quest, bool complete, byte[] rawSaveFile)
+        {
+            int offset = Constants.QUESTS_SECTION_OFFSET + GetQuestOffset(difficulty, act, quest);
+
+            if (offset == -1)
+            {
+                return;
+            }
+
+            if (complete)
+            {
+                rawSaveFile[offset] = 0x01;     // Quest complete
+                rawSaveFile[offset + 1] = 0x10; // Quest log animation viewed
+
+                if (act == Constants.Act.LordOfDestruction && quest == Constants.Quest.ThirdQuest)
+                {
+                    // Scroll of resist
+                    rawSaveFile[offset] += 0xC0;
+                }
+            }
+            else
+            {
+                rawSaveFile[offset] = 0;
+                rawSaveFile[offset + 1] = 0;
+            }
+
+            // Allow travel to the next act.
+            // For Act4, the diablo quest is quest2
+            if (complete && (quest == Constants.Quest.SixthQuest || (act == Constants.Act.TheHarrowing && quest == Constants.Quest.SecondQuest)))
+            {
+                if (act != Constants.Act.TheHarrowing)
+                {
+                    rawSaveFile[offset + 2] = 1;
+                }
+                else
+                {
+                    rawSaveFile[offset + 4] = 1;
+                }
+            }
+        }
+
         private static void UpdateChecksum(byte[] fileData)
         {
-            if (fileData == null || fileData.Length < CHECKSUM_OFFSET + 4) return;
+            if (fileData == null || fileData.Length < Constants.CHECKSUM_OFFSET + 4) return;
 
             // Clear out the old checksum
-            Array.Clear(fileData, CHECKSUM_OFFSET, 4);
+            Array.Clear(fileData, Constants.CHECKSUM_OFFSET, 4);
 
             int[] checksum = new int[4];
             bool carry = false;
@@ -126,9 +225,9 @@ namespace D2ROffline.Tools
                 carry = (checksum[3] & 128) != 0;
             }
 
-            for (int i = CHECKSUM_OFFSET; i < CHECKSUM_OFFSET + 4; ++i)
+            for (int i = Constants.CHECKSUM_OFFSET; i < Constants.CHECKSUM_OFFSET + 4; ++i)
             {
-                fileData[i] = (byte)checksum[i - CHECKSUM_OFFSET];
+                fileData[i] = (byte)checksum[i - Constants.CHECKSUM_OFFSET];
             }
         }
     }
